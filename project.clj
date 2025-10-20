127b989418bd98967eae10e35e0d660fc13b5e97
Haokang Den <haokang.den@gmail.com>
2025-10-20 10:47:45 +0900

    add CI

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject labrador "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A dataloader library based on Urania"
   :url "https://github.com/hden/labrador"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.2"]
                  [funcool/promesa "11.0.678"]
                  [funcool/urania "0.2.0"]]
-  :repl-options {:init-ns labrador.core})
+  :repl-options {:init-ns labrador.core}
+  :plugins [[lein-cloverage "1.2.4"]])
